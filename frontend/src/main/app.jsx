8b549683f2b09f8f9aa92b6ffc619f9546731758
joao front <joao.saousa@tangotech.com.br>
2019-12-31 16:42:39 -0300

    todo app funcionando

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,12 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+import '../template/custom.css'
+
+import React from 'react'
+
+
+export default props => (
+    <div className='container'>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus necessitatibus, ducimus iste voluptatem nulla eius ab ipsa alias omnis inventore!</p>
+    </div>
+)
