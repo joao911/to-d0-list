430a785b7295652ea9b1672bed976e0177b282d0
joao front <joao.saousa@tangotech.com.br>
2020-01-02 06:05:36 -0300

    falta pouco

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+     <div>
+          <PageHeader name='Sobre' small='Noś'></PageHeader>
+
+          <h2>Nossa História</h2>
+          <p>Esse aplicativo está sendo feitto com o intuito de se aprofundar no React native e ter mais indepêndencia dos plenos. <br/> inclusive do Sr Marivaldo Sena que está sobrecarregado em 3 projetos</p>
+          <h2>Missão e visão</h2>
+          <p>Buscar minha senioridade</p>
+          <h2>Imprensa</h2>
+          <p>todas as duvidas desse projeto foram sanadas pelo SR Marivaldo Sena</p>
+     </div>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,12 +1,14 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
-import '../template/custom.css'
 
 import React from 'react'
-
+import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
-    <div className='container'>
-       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus necessitatibus, ducimus iste voluptatem nulla eius ab ipsa alias omnis inventore!</p>
+    <div className="container">
+        <Menu/>
+       <Routes/>
     </div>
+
 )
